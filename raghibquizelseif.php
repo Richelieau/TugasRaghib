ee9c274a43e875664b9cd28584fe8204d20078db
Richelieau <abiyyudzakiraghib@gmail.com>
2025-04-28 09:03:35 +0800

    Quiz raghib!

---
+++ raghibquizelseif.php
@@ -0,0 +1,10 @@
+<?php
+$nilai_matematika = 65;
+$nilai_bahasa = 55;
+
+if ($nilai_matematika >= 80 && $nilai_bahasa >= 70) {
+    echo "Selamat! Kamu boleh nonton Film setelah ini!";
+} else {
+    echo "Ckckck... belajar lagi ya, terus berusaha!";
+}
+?>
