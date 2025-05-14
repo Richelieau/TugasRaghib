bd03a6ca89fd5ece0ba4641daa45d6201ff9dd19
Richelieau <abiyyudzakiraghib@gmail.com>
2025-05-14 11:25:11 +0800

    Tugas Praktik FOR, DO, WHILE.

---
+++ raghib10.php
@@ -0,0 +1,7 @@
+<?php
+
+$i = 0;
+while ($i < 8){
+    echo "Raghib Abiyyudzky<br>";
+    $i++;
+}
---
+++ raghib11.php
@@ -0,0 +1,7 @@
+<?php
+
+$i = 1;
+do {
+    echo "Raghib Abiyyudzaky";
+    $i++;
+} while ($i < 8);
