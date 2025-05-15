b98a9d699e4baf691411ed7804c3918197970dbb
Richelieau <abiyyudzakiraghib@gmail.com>
2025-05-15 14:02:22 +0800

    Tugas Array raghib

---
+++ Arrayraghib.php
@@ -0,0 +1,10 @@
+<?php
+// $buah = ["mangga", "jeruk", "durian", "apel"];
+// $sayur = array("tomat", "kangkung", "sawi", "buncis");
+
+// echo "$sayur[2]";
+$daftar =["Aerostreet", " Compass", "Ortus8", "Specs"];
+ echo "Daftar Koleksi Sepatu</br>";
+ foreach ($daftar as $DaftarKoleksi) {
+    echo "-".$DaftarKoleksi."</br>";
+ }
